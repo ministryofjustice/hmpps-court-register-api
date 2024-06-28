a92458043c646aaa0c70348c3b727503061953fd
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-06-28 09:57:10 +0100

    RASS-263 make refresh idempotent

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscourtregisterapi/service/SDRSService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscourtregisterapi/service/SDRSService.kt
@@ -10,6 +10,7 @@ import uk.gov.justice.digital.hmpps.hmppscourtregisterapi.jpa.Contact
 import uk.gov.justice.digital.hmpps.hmppscourtregisterapi.jpa.ContactRepository
 import uk.gov.justice.digital.hmpps.hmppscourtregisterapi.jpa.Court
 import uk.gov.justice.digital.hmpps.hmppscourtregisterapi.jpa.CourtRepository
+import uk.gov.justice.digital.hmpps.hmppscourtregisterapi.jpa.CourtType
 import uk.gov.justice.digital.hmpps.hmppscourtregisterapi.jpa.CourtTypeRepository
 
 @Service
@@ -17,12 +18,28 @@ class SDRSService(private val sdrsApiClient: SDRSApiClient, private val courtTyp
 
   fun refreshData() {
     val courtData = sdrsApiClient.callSDRS(SDRSRequest())
-    courtData.messageBody.gatewayOperationType.organisationUnitResponse.organisationUnit.forEach {
-      val court = Court.from(it)
-      court.courtType = courtTypeRepository.findByIdOrNull(court.courtType.id) ?: courtTypeRepository.save(court.courtType)
+    courtData.messageBody.gatewayOperationType.organisationUnitResponse.organisationUnit.forEach { organisationUnit ->
+      val courtType = CourtType.from(organisationUnit.oUCodeL1Name)
+      val savedCourtType = courtTypeRepository.findByIdOrNull(courtType.id) ?: courtTypeRepository.save(courtType)
+      val court = courtRepository.findByIdOrNull(organisationUnit.oUCode)?.let { court ->
+        court.updateFromOrganisationUnit(organisationUnit, savedCourtType)
+        courtRepository.save(court)
+      } ?: Court.from(organisationUnit, savedCourtType)
+
+      court.courtType = savedCourtType
       val savedCourt = courtRepository.save(court)
-      val building = buildingRepository.save(Building.from(it, savedCourt))
-      it.phoneNumber?.let { phoneNumber -> contactRepository.save(Contact(building = building, type = "TEL", detail = phoneNumber)) }
+      val building = court.buildings.firstOrNull { building -> building.postcode?.equals(organisationUnit.postCode) == true }?.let { building ->
+        building.updateFromOrganisationUnit(organisationUnit)
+        buildingRepository.save(building)
+      } ?: buildingRepository.save(Building.from(organisationUnit, savedCourt))
+
+      organisationUnit.phoneNumber?.let { phoneNumber ->
+        building.contacts.firstOrNull { contact -> contact.detail.equals(phoneNumber) }?.let { contact ->
+          contact.type = "TEL"
+          contact.detail = phoneNumber
+          contactRepository.save(contact)
+        } ?: contactRepository.save(Contact(building = building, type = "TEL", detail = phoneNumber))
+      }
     }
   }
 }
