5595202f7a8d026a5d60b98408525d0af72fef72
Carlo Veo <carlo.veo2@justice.gov.uk>
2026-01-28 14:10:05 +0000

    fixes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscourtregisterapi/client/sdrs/dto/SDRSResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppscourtregisterapi/client/sdrs/dto/SDRSResponse.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.hmppscourtregisterapi.client.sdrs.dto
 import tools.jackson.databind.PropertyNamingStrategies
 import tools.jackson.databind.annotation.JsonNaming
 
-
 @JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy::class)
 data class SDRSResponse(
   val messageBody: MessageBodyResponse,
