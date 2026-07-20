dba2eb8dabacbbead61ad8ccce32b6819d2b7a41
Joel Stobart (MOJ) <87521440+joelstobart-moj@users.noreply.github.com>
2026-07-20 16:05:59 +0100

    6 courts need new associations (#198)

---
+++ src/main/resources/migration/common/V4_1__add_missing_courts.sql
@@ -0,0 +1,23 @@
+update court
+set cp_court_uuid = '76313f91-4103-37d0-9eae-ce5bdf652986'
+where id = 'SLOUMC';
+
+update court
+set cp_court_uuid = '40725a45-9ded-3c1b-908f-88b6a766fe40'
+where id = 'TAMSMC';
+
+update court
+set cp_court_uuid = '16ed5e59-40bb-3e05-b525-4ddfbb8fca13'
+where id = 'WELBMC';
+
+update court
+set cp_court_uuid = '69e474a4-3590-3731-ac3b-79835d76a4bb'
+where id = 'GTSHMC';
+
+update court
+set cp_court_uuid = 'bfe68b2a-16c7-37dc-997f-5b4ada947d28'
+where id = 'CWMBMC';
+
+update court
+set cp_court_uuid = 'ea023380-f927-49e0-ab09-2f08627cb127'
+where id = 'HNDNMC';
