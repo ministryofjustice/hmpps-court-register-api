65a9fe12df3af23516a6d980e7b4205ce3e47ef5
Joel Stobart <joel.stobart@justice.gov.uk>
2026-07-22 09:21:26 +0100

    used dumb column name :)

--- src/main/resources/migration/common/V4_2__associate_missing_courts_2.sql
+++ src/main/resources/migration/common/V4_2__associate_missing_courts_2.sql
@@ -1,39 +1,39 @@
 -- Chesterfield Justice Centre
 UPDATE court
-SET court_uuid = '01cfe611-ec22-3e55-b212-aa3230c461cb'
+SET cp_court_uuid = '01cfe611-ec22-3e55-b212-aa3230c461cb'
 WHERE id = 'CHDJUC';
 
 -- Teesside Magistrates Court
 UPDATE court
-SET court_uuid = '26ed2f88-a94c-3ad6-95c4-daede2e673d5'
+SET cp_court_uuid = '26ed2f88-a94c-3ad6-95c4-daede2e673d5'
 WHERE id = 'TEEMAJ';
 
 -- Isle of Wight Combined Court
 UPDATE court
-SET court_uuid = '2d5444ea-a5cf-3c69-8d8b-8993397724f3'
+SET cp_court_uuid = '2d5444ea-a5cf-3c69-8d8b-8993397724f3'
 WHERE id = 'IOWCCC';
 
 -- Cardiff MC - Crown Court Docks
 UPDATE court
-SET court_uuid = '39fe3360-b3b5-3285-a3df-a75feb4e463c'
+SET cp_court_uuid = '39fe3360-b3b5-3285-a3df-a75feb4e463c'
 WHERE id = 'CMCCCD';
 
 -- Tameside Magistrates Court
 UPDATE court
-SET court_uuid = '40725a45-9ded-3c1b-908f-88b6a766fe40'
+SET cp_court_uuid = '40725a45-9ded-3c1b-908f-88b6a766fe40'
 WHERE id = 'TAMMAJ';
 
 -- Caernarfon Justice Centre
 UPDATE court
-SET court_uuid = '4adfe0cb-b2bd-3436-bab9-1e925926dd9c'
+SET cp_court_uuid = '4adfe0cb-b2bd-3436-bab9-1e925926dd9c'
 WHERE id = 'CAECOM';
 
 -- Newcastle-upon-Tyne Combined Court Centre
 UPDATE court
-SET court_uuid = '8de7f2e2-5705-3be5-af9a-321a891ab708'
+SET cp_court_uuid = '8de7f2e2-5705-3be5-af9a-321a891ab708'
 WHERE id = 'NUTCCC';
 
 -- Wirral Magistrates Court
 UPDATE court
-SET court_uuid = '9d5798b5-1fda-34cb-89de-3317f9b24c3e'
+SET cp_court_uuid = '9d5798b5-1fda-34cb-89de-3317f9b24c3e'
 WHERE id = 'WIRMAJ';
