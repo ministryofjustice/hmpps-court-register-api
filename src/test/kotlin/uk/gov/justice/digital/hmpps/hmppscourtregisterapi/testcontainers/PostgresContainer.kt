f9800fafa38370c2d19276156a4c1cee660a4b8a
Carlo Veo <144013256+carloveo-moj@users.noreply.github.com>
2026-09-18 13:24:14 +0100

    RASS-2666 upgrade postgres to 18 (#220)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppscourtregisterapi/testcontainers/PostgresContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppscourtregisterapi/testcontainers/PostgresContainer.kt
@@ -17,7 +17,7 @@ object PostgresContainer {
 
     val logConsumer = Slf4jLogConsumer(log).withPrefix("postgresql")
 
-    return PostgreSQLContainer<Nothing>("postgres:16").apply {
+    return PostgreSQLContainer<Nothing>("postgres:18").apply {
       withEnv("HOSTNAME_EXTERNAL", "localhost")
       withDatabaseName("court_register_db")
       withUsername("admin")
